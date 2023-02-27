a46c096db4a490c0e14866462d888a385ad85d6a
Ramchandra Warang <samuel.warang@gmail.com>
2023-02-27 22:54:07 +0530

    adding basic number guessing code

--- Main.java
+++ Main.java
@@ -1,5 +1,22 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello Ramchandra");
-    }       
+        int answer, guess;
+
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+
+        answer = rand.nextInt(10) + 1;
+
+        System.out.println("Guess a number between 1 & 10: ");
+        guess = sc.nextInt();
+
+        System.out.println(
+                (guess == answer) ? "Good job the number was : " + answer
+                        : "Sorry, but the right number was :" + answer);
+
+    }
 }
